e1744bd3e369e0eaf067e33c9cad699fdfcbdbeb
Вячеслав Ветошкин <vva6699@gmail.com>
2023-08-10 22:00:37 +0700

    fix

---
+++ DocumentManagementSystem/Client/Models/Controller.cs
@@ -0,0 +1,29 @@
+﻿using DocumentManagementSystem.Client.Components.OpenAPI;
+using DocumentManagementSystem.Shared.OpenApi;
+using Microsoft.AspNetCore.Components;
+
+namespace DocumentManagementSystem.Client.Models
+{
+    public struct Controller
+    {
+        public Controller(string name, PathItem pathItem)
+        {
+            Name = name;
+            IsOpen = true;
+            Actions = new() { pathItem };
+        }
+
+        /// <summary>
+        /// Название контроллера
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Флаг, инфа о контроллере открыта
+        /// </summary>
+        public bool IsOpen { get; set; }
+        /// <summary>
+        /// Список действий
+        /// </summary>
+        public List<PathItem> Actions { get; set; }
+    }
+}
