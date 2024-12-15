c9c883115ba082c0ae280f1d762d594d8377354b
Вячеслав Ветошкин <vva6699@gmail.com>
2024-12-15 23:23:46 +0700

    Добавил обработку ошибки авторизации

--- DocumentManagementSystem/Client/Providers/CustomHttpHandler.cs
+++ DocumentManagementSystem/Client/Providers/CustomHttpHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net;
 
 namespace DocumentManagementSystem.Client.Providers
 {
@@ -12,19 +13,38 @@ namespace DocumentManagementSystem.Client.Providers
         }
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            if (request.RequestUri.AbsolutePath.ToLower().Contains("login") ||
-                request.RequestUri.AbsolutePath.ToLower().Contains("registration"))
+            var pathToLower = request.RequestUri.AbsolutePath.ToLower();
+
+            if (pathToLower.Contains("refreshaccesstoken") ||
+                pathToLower.Contains("login") ||
+                pathToLower.Contains("registration"))
             {
                 return await base.SendAsync(request, cancellationToken);
             }
 
             var token = await _authStateProvider.GetJWTAccessToken();
+
             if (!string.IsNullOrEmpty(token))
-            {
                 request.Headers.Add("Authorization", $"Bearer {token}");
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response is not null && response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                _authStateProvider.ResetAuthState();
+                request.Headers.Remove("Authorization");
+
+                token = await _authStateProvider.GetJWTAccessToken();
+
+                if (!string.IsNullOrEmpty(token))
+                {
+                    request.Headers.Add("Authorization", $"Bearer {token}");
+
+                    response = await base.SendAsync(request, cancellationToken);
+                }
             }
 
-            return await base.SendAsync(request, cancellationToken);
+            return response;
         }
     }
 }
